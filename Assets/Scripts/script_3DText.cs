9ddf159451db19e5b16c7538b42663255895f749
Torben <torbenvdh@gmail.com>
2019-08-01 18:32:17 +0200

    3d Text instructions on interactions, Lighting Changes

---
+++ Assets/Scripts/script_3DText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_3DText : MonoBehaviour
+{
+    MeshRenderer comp_TextMesh;
+    Collider comp_Collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        comp_TextMesh = GetComponent<MeshRenderer>();
+        comp_Collider = GetComponent<Collider>();
+
+        comp_TextMesh.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            comp_TextMesh.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            comp_TextMesh.enabled = false;
+        }
+    }
+}
